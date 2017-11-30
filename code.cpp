4f6554dab91b6379f8252e42049ab096cc14608d
Mohiuddin <mohiuddinrazu@gmail.com>
2017-11-30 20:53:05 +0600

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,28 @@
+// Fibonacci series using golden ratio
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double goldenratio (int n=100){
+	double x = 1;
+	while (n >=0){
+		x = 1 + 1/x;
+		n--;
+	}
+	return x;
+}
+
+double fibonacci (int n) {
+	double a = pow(goldenratio(100), n+1) - pow(1-goldenratio(100), n+1);
+	double b = 2*goldenratio(100) - 1;
+	return a/b;
+}
+
+int main(){
+	cout << "The golden ratio is: " << goldenratio(100) << endl;
+	cout << "Fibonacci series: " << endl;
+	for(int i=0 ; i <= 11 ; i++){
+		cout << fibonacci(i) << endl;
+	}
+}
